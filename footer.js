a787fc28c0cb31b4352e9deb7b7f6ce76c154530
Brendan Beard <46061381+AtlasDisease@users.noreply.github.com>
2022-02-10 22:36:35 -0600

    Version: FU 2.4.0 Beta
    - Added some JS

---
+++ footer.js
@@ -0,0 +1,4 @@
+document.write(`
+<a href="">&#8593 Back to Top</a>
+<p>© 2018-2022 AriesTechnologies LLC</p>
+`);
---
+++ spartanos/header.js
@@ -0,0 +1,8 @@
+document.write(`
+   <div class="navigation">
+      <img src="../Images/icons/SpartanOS.png" onclick="window.location.href='home.html'" />
+      <a href="news.html">NEWS</a>
+      <a href="docs.html">DOCS</a>
+         <a href="../store/spartanos.html">STORE</a>
+   </div>
+   `);
