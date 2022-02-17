8efc95f909209c37017bbf1dd364240cd40d5261
Brendan Beard <46061381+AtlasDisease@users.noreply.github.com>
2022-02-17 17:12:34 -0600

    Version: FU 5.0.0 Beta
    - Revised SpartanOS website
    - Added a small amount of info about {SpartanOS 2024}
    - Fixed bug with footer

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 document.write(`
    <footer>
       <a href="">&#8593 Back to Top</a>
-      <p>© 2018-2022 AriesTechnologies LLC</p>
+      <p>&#169; 2018-2022 AriesTechnologies LLC</p>
    </footer>
 `);
--- spartanos/header.js
+++ spartanos/header.js
@@ -2,9 +2,10 @@ document.write(`
    <header>
       <div class="navigation">
          <img src="../Images/icons/SpartanOS.png" onclick="window.location.href='home.html'" />
-         <a href="news.html">NEWS</a>
+         <a href="versions.html">VERSIONS</a>
          <a href="docs.html">DOCS</a>
-            <a href="../store/spartanos.html">STORE</a>
+         <a href="../store/spartanos.html">STORE</a>
+         <a href="support.html">SUPPORT</a>
       </div>
    </header>
    `);
