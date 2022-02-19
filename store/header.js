6c16b2c1883a4438a2df9d6311fd0a2bf177806c
Brendan Beard <46061381+AtlasDisease@users.noreply.github.com>
2022-02-19 17:50:16 -0600

    Version: IU 5.2.0 Beta
    - Added inability to right-click
    - Added header.js for Store (this is temporary until I find a way to do a different thing)

---
+++ store/header.js
@@ -0,0 +1,9 @@
+document.write(`
+	<header>
+		<div class="navigation">
+			<a href="home.html">HOME</a>
+			<a href="apps.html">APPS</a>
+			<a href="spartanos.html">SPARTANOS</a>
+		</div>
+	</header>
+`);
