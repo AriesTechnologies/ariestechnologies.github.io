1acbfd8001a245a1a8422b5bec5cb578ab14d424
Brendan Beard <46061381+AtlasDisease@users.noreply.github.com>
2022-04-30 11:52:50 -0500

    Version: IU 7.1.3 Beta - More testing for "Mobile Update"

--- spartanos/header.js
+++ spartanos/header.js
@@ -8,6 +8,22 @@ document.write(`
          <a href='support.html'>SUPPORT</a>
          <button id='theme' type='button'></button>
       </div>
+
+      <!-- For Mobile -->
+      <div class="topnav">
+         <!--<img src="../Images/icons/SpartanOS.png'" onclick="window.location.href='index.html'" />-->
+         <a href="javascript:void(0);" id="menu" class="icon">
+            <i class="fa fa-bars"></i>
+         </a>
+         <div id="links">
+            <a href="home.html">HOME</a>
+            <a href='versions.html'>VERSIONS</a>
+            <a href='docs.html'>DOCS</a>
+            <a href='../store/spartanos.html'>STORE</a>
+            <a href='support.html'>SUPPORT</a>
+            <button id='theme2' type='button'></button>
+         </div>
+      </div>
    </header>
    `);
 
@@ -52,6 +68,16 @@ document.addEventListener('DOMContentLoaded', function () {
       '(30,30,30,255)', //Almost Black
    ];
 
+   // Hamburger Menu
+   document.querySelector('#menu').addEventListener('click', function () {
+      const x = document.getElementById("links");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+   });
+
    const changeTheme = function (num) {
       themeNum += 1;
       if (themeNum >= themes.length) {
@@ -62,12 +88,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
       const theme = "rgb"+themes[themeNum];
       document.querySelector('#theme').textContent = themeNames[themeNum];
+      document.querySelector('#theme2').textContent = themeNames[themeNum];
       document.querySelectorAll('button').forEach( function (item) {
          item.style.background = theme;
       });
    };
 
    document.querySelector('#theme').addEventListener('click', function () {
+      changeTheme(themeNum);
+   });
+   document.querySelector('#theme2').addEventListener('click', function () {
       changeTheme(themeNum);
    });
 
