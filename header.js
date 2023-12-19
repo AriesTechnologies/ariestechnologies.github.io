7f864d127d5d2fe57394bf9443a1cba57e375061
Brendan <46061381+AtlasDisease@users.noreply.github.com>
2023-12-19 11:46:40 -0600

    Update
    Improvements to website for small screens

--- header.js
+++ header.js
@@ -1,24 +1,11 @@
 document.write(`
    <header>
       <div class="navigation">
-         <img src="Images/icons/AriesTech.svg" onclick="window.location.href='index.html'" />
-         <a href="news.html">NEWS</a>
-         <a href="spartanos/home.html" target="_blank" rel="noopener noreferrer">SPARTANOS</a>
-         <a href="spartanos/acfs.html" target="_blank" rel="noopener noreferrer">ACFS</a>
-         <a href="otherprojects.html">OTHER PROJECTS</a>
-         <a href="store/home.html" target="_blank" rel="noopener noreferrer">STORE</a>
-         <a href="support.html">SUPPORT</a>
-      </div>
-
-      <!-- For Mobile -->
-      <div class="topnav">
-         <!--<img src="Images/icons/AriesTech.svg" class="active" />-->
-         <!--<a href="#home" class="active">LOGO</a>-->
          <a href="javascript:void(0);" id="menu" class="icon">
             <i class="fa fa-bars"></i>
          </a>
          <div id="links">
-            <a href="index.html">HOME</a>
+            <img id="nav-img" src="Images/icons/AriesTech.svg" onclick="window.location.href='index.html'" />
             <a href="news.html">NEWS</a>
             <a href="spartanos/home.html" target="_blank" rel="noopener noreferrer">SPARTANOS</a>
             <a href="spartanos/acfs.html" target="_blank" rel="noopener noreferrer">ACFS</a>
@@ -27,7 +14,6 @@ document.write(`
             <a href="support.html">SUPPORT</a>
          </div>
       </div>
-   </header>
    `);
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -36,10 +22,14 @@ document.addEventListener('DOMContentLoaded', function () {
    // Hamburger Menu
    document.querySelector('#menu').addEventListener('click', function () {
       const x = document.getElementById("links");
-      if (x.style.display === "block") {
+      if (x.style.display != "none" && x.style.display != "") {
         x.style.display = "none";
       } else {
         x.style.display = "block";
       }
+   });
+
+   addEventListener('resize', function () {
+      document.getElementById("links").style.removeProperty("display");
    });
 });
