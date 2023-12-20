1e81333e7dd49c0461609be3d7b06565fc960d3b
Brendan <46061381+AtlasDisease@users.noreply.github.com>
2023-12-20 16:01:40 -0600

    Update
    Update

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 document.write(`
    <footer>
       <a href="">&#8593 Back to Top</a>
-      <p>&#169; 2018-2022 AriesTechnologies LLC</p>
+      <p>&#169; 2018-2023 AriesTechnologies LLC</p>
    </footer>
 `);
